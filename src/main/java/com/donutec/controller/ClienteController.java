a86de752eb112a87474b169355c117cfb8ecb241
jenry <jenrycn02@gmail.com>
2025-09-02 01:36:57 -0300

    listar clientes na tabela

--- src/main/java/com/donutec/controller/ClienteController.java
+++ src/main/java/com/donutec/controller/ClienteController.java
@@ -5,8 +5,14 @@ import com.donutec.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/clientes")
@@ -15,9 +21,22 @@ public class ClienteController {
     @Autowired
     ClienteService clienteService;
 
+    @GetMapping("/gerenciar")
+    public String gerenciar(Cliente cliente, Model model){
+        model.addAttribute("clientes",clienteService.listar());
+        return "cliente/clientes";
+    }
+
+
     @PostMapping("salvar")
     private String salvar(@Valid Cliente cliente){
         clienteService.salvar(cliente);
+        return "cliente/cadastro";
+    }
+
+    @GetMapping("/deletar")
+    private String deletar(@PathVariable("id")Long id, @Valid Cliente cliente){
+        clienteService.deletar(id);
         return "cliente/cadastro";
     }
 }
--- src/main/java/com/donutec/service/ClienteService.java
+++ src/main/java/com/donutec/service/ClienteService.java
@@ -5,6 +5,9 @@ import com.donutec.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ClienteService {
 
@@ -13,6 +16,14 @@ public class ClienteService {
 
     public Cliente salvar(Cliente cliente){
         return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listar(){
+        return clienteRepository.findAll();
+    }
+
+    public void deletar(Long id){
+        clienteRepository.deleteById(id);
     }
 
 }
