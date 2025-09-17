18d0b26c2ebe6e62375fc8ae6e393dbef4f9842a
jenry <jenrycn02@gmail.com>
2025-09-17 02:50:49 -0300

    autocomplete por nome com ajax

---
+++ src/main/java/com/donutec/dto/ClienteDTO.java
@@ -0,0 +1,35 @@
+package com.donutec.dto;
+
+import com.donutec.model.Cliente;
+
+public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+
+    public ClienteDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public ClienteDTO(Cliente cliente){
+        id = cliente.getId();
+        nome = cliente.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/com/donutec/service/ClienteService.java
+++ src/main/java/com/donutec/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.donutec.service;
 
+import com.donutec.dto.ClienteDTO;
 import com.donutec.model.Cliente;
 import com.donutec.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ClienteService {
@@ -31,5 +33,10 @@ public class ClienteService {
          return clienteRepository.findById(id);
     }
 
+    public List<ClienteDTO> buscarPorNome(String nome){
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes.stream().filter(x -> x.getNome().toLowerCase().contains(nome.toLowerCase()))
+                .collect(Collectors.toList()).stream().map(z -> new ClienteDTO(z)).toList();
+    }
 
 }
