b34e397bbabece3cb829ec4a2c085b9182e58d8e
jenry <jenrycn02@gmail.com>
2025-09-17 03:28:44 -0300

    dataVenda

--- src/main/java/com/donutec/model/Venda.java
+++ src/main/java/com/donutec/model/Venda.java
@@ -3,6 +3,9 @@ package com.donutec.model;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Venda {
@@ -12,9 +15,12 @@ public class Venda {
     private Long id;
     @ManyToOne
     private Cliente cliente;
+    @OneToMany(mappedBy = "venda", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItensVenda> itensVenda = new ArrayList<>();
     private String formaPagamento;
     private String enderecoEntrega;
     private String observacao;
+    private LocalDateTime dataVenda;
     private BigDecimal valorTotal;
 
     public Long getId() {
@@ -31,6 +37,14 @@ public class Venda {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public List<ItensVenda> getItensVenda() {
+        return itensVenda;
+    }
+
+    public void setItensVenda(List<ItensVenda> itensVenda) {
+        this.itensVenda = itensVenda;
     }
 
     public String getFormaPagamento() {
@@ -55,6 +69,14 @@ public class Venda {
 
     public void setObservacao(String observacao) {
         this.observacao = observacao;
+    }
+
+    public LocalDateTime getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDateTime dataVenda) {
+        this.dataVenda = dataVenda;
     }
 
     public BigDecimal getValorTotal() {
